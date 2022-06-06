777a120b3fc53cd8f13bd61b67e33616d27d2bd4
YunJaem06 <96619472+YunJaem06@users.noreply.github.com>
2022-06-06 11:57:30 +0900

    [Bronze II] Title: 16진수, Time: 116 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		
+		int b = Integer.parseInt(a, 16);
+		
+		System.out.println(b);
+	}	
+}
