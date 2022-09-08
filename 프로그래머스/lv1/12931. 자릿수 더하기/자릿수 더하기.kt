5bc69a023f685056c3e586b24614615ec41e9ebc
YunJaem06 <96619472+YunJaem06@users.noreply.github.com>
2022-09-08 20:42:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer = 0
+        var input = n
+
+        while (input != 0){
+            answer += input % 10
+            input /= 10
+        }
+        return answer
+    }
+}
