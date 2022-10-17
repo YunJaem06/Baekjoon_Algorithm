a18f90249e468ae98f9758e1835c351882356c08
YunJaem06 <96619472+YunJaem06@users.noreply.github.com>
2022-10-18 08:47:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.37 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
@@ -4,7 +4,7 @@ class Solution {
         var cnt = 0
         for (i in s.indices){
             if (s[i] == ' ') cnt = -1
-            answer += if (cnt%2 == 0) s[i].toUpperCase() else s[i].toLowerCase()
+            answer += if (cnt%2 == 0) s[i].uppercase() else s[i].lowercase()
             cnt++
         }
         return answer
