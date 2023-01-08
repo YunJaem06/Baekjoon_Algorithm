4baef398a8de8b378e592e1fefc1ca83f0c12b27
Viva <96619472+YunJaem06@users.noreply.github.com>
2023-01-08 20:59:34 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.14 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer = 0
+        for (i in 0 .. t.length - p.length) {
+            val sub = t.substring(i, i + p.length)
+
+            if (sub.toLong() <= p.toLong()) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
