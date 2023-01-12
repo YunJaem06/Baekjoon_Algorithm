e3eb415d51b250345464cfe3f9334c984dd6558f
Viva <96619472+YunJaem06@users.noreply.github.com>
2023-01-12 20:54:48 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 23.92 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(s: String): IntArray {
+        var answer = IntArray(2)
+        var str = s
+        while (str != "1") {
+            answer[0]++
+            answer[1] += str.replace("1", "").length
+            str = str.replace("0", "").length.toString(2)
+        }
+        return answer
+    }
+}
