a8baad7257860f5e50a1720832f7f66c92c5a599
Viva <96619472+YunJaem06@users.noreply.github.com>
2023-01-19 19:32:27 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
-        var a = num1
-        var b = num2
         var answer = 0
         if(num1 == num2) {
             answer = 1
